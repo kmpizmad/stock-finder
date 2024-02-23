c7180ac77a5f7cc5d2e1305b511974a51aa8da9e
kmpizmad <nagy.viktor392@gmail.com>
2024-02-23 22:16:56 +0100

    feat: added animations

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,17 +1,24 @@
 'use client';
 
-import { useMemo, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import { ChevronsRight, Crown, TextSearch } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 
-type ActionGroupObject = { id: string; icon: JSX.Element; link: string };
+type ActionGroupObject = {
+  id: string;
+  label: string;
+  icon: JSX.Element;
+  link: string;
+};
 
 const iconSize: number = 20;
 const actionGroups: ActionGroupObject[][] = [
   [
     {
       id: 'group-icon-search',
+      label: 'Search stocks',
       icon: (
         <TextSearch
           width={iconSize}
@@ -22,6 +29,7 @@ const actionGroups: ActionGroupObject[][] = [
     },
     {
       id: 'group-icon-crown',
+      label: 'Favorite stocks',
       icon: (
         <Crown
           width={iconSize}
@@ -34,27 +42,40 @@ const actionGroups: ActionGroupObject[][] = [
 ];
 
 export default function Sidebar(): JSX.Element {
-  const [selectedId, setSelectedId] = useState<string>(actionGroups[0][0].id);
+  const pathname = usePathname();
+  const [selectedId, setSelectedId] = useState<string>(
+    actionGroups.flat().find(item => item.link === pathname)?.id || actionGroups[0][0].id
+  );
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <aside className="fixed top-0 bottom-0 left-0 flex flex-col items-center justify-between border-r bg-background border-border">
-      <div>
+      <div
+        className={cn('grid grid-cols-[0fr] overflow-hidden transition-all duration-300', {
+          'grid-cols-[1fr]': isOpen,
+        })}
+      >
         {actionGroups.map((group, index) => {
           return (
             <ul
               key={`action-group-${index}`}
-              className="flex flex-col items-center justify-around py-2 border-b border-border"
+              className="flex flex-col items-center justify-around py-2 border-b min-w-[4.25rem] border-border"
             >
               {group.map(item => {
                 return (
                   <li
                     key={item.id}
-                    className={cn('nav-item px-6', { active: item.id === selectedId })}
+                    className={cn('nav-item px-6 w-full flex items-center overflow-hidden', {
+                      'active': item.id === selectedId,
+                      'gap-2': isOpen,
+                      'gap-6': !isOpen,
+                    })}
                     onClick={() => {
                       setSelectedId(item.id);
                     }}
                   >
                     <Link href={item.link}>{item.icon}</Link>
+                    <div className="text-sm">{item.label}</div>
                   </li>
                 );
               })}
@@ -62,9 +83,21 @@ export default function Sidebar(): JSX.Element {
           );
         })}
       </div>
-      <div className="flex justify-center w-full nav-item">
-        <ChevronsRight />
+      <div
+        className={cn('flex w-full nav-item', {
+          'justify-end': isOpen,
+          'justify-center': !isOpen,
+        })}
+        onClick={() => {
+          setIsOpen(prev => !prev);
+        }}
+      >
+        <div className="px-3">
+          <ChevronsRight />
+        </div>
       </div>
     </aside>
   );
 }
+
+type SidebarProps = { pathname: string };
